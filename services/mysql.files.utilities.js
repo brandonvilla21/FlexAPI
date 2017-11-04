1d8065423c389e9ffc0b9176ad2d550750452710
Marcos Barrera <elyomarcos@gmail.com>
2017-11-04 12:10:15 -0600

    Refactored and added new fuctions to build the correct '/restore' endpoint.

--- services/mysql.files.utilities.js
+++ services/mysql.files.utilities.js
@@ -1,5 +1,8 @@
 const mysqlDump = require('mysqldump');
 const multer = require('multer');
+const async = require('async');
+const importer = require('node-mysql-importer');
+const mysql = require('mysql');
 
 let MysqlUtilities = {};
 
@@ -21,19 +24,87 @@ MysqlUtilities.backup = (res, options) => {
 
 MysqlUtilities.restore = (req, res, options) => {
 
-  
-  uploadRestoreFile(req, res, options);
+  async.waterfall([
+    cb => { setFileEnvironment(req, res, options, cb) },
+    (req, res, upload, cb) => { importDatabase(req, res, options.db.username, options.db.password, upload, cb) },
+    // (req, res, upload, cb) => { upload(req, res, upload, cb) },
+  ],
+
+    error => {
+      if (error) res.status(500).json(error);
+      res.status(200).json({ message: "Success" });
+      console.log("Success en Waterfall :v");
+    });
+  // importDatabase(options.db.username, options.db.password);
 
 }
 
-function uploadRestoreFile( req, res, options ) {
+
+function importDatabase(req, res, username, password, upload, cb) {
+
+  importer.config({
+    'host': process.env.DB_HOST,
+    'user': username,
+    'password': password,
+    'database': process.env.DB_NAME
+  });
+
+  const conn = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: username,
+    password: password,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    multipleStatements: process.env.MULTI_STATEMENTS
+
+  });
+
+  conn.connect(err => {
+    if (err) {
+      console.log('Error trying to connect with Data Base: ' + err.stack);
+      return cb(err);
+    }
+
+    console.log('Valid Credentials');
+
+
+    try {
+
+      //Importing database.
+      importer.importSQL('./temp/uploadedRestore.sql')
+        .then(() => {
+          upload(req, res, err => {
+            console.log(req.file);
+
+            if (err) return cb(err);
+            conn.destroy();
+            return cb(null);
+          });
+
+        }).catch(err => {
+          console.log(`error on Promise.catch: ${err}`);
+          conn.destroy();
+
+          return cb(err);
+        })
+    } catch (error) {
+      console.log(`error on try.catch: ${error}`);
+      return cb(error);
+    }
+  });
+
+}
+
+
+function setFileEnvironment(req, res, options, cb) {
+
   let storage = multer.diskStorage({ //multers disk storage settings
     destination: (req, file, cb) => {
       cb(null, options.directory);
     },
     filename: (req, file, cb) => {
       const datetimestamp = Date.now();
-      cb(null, file.fieldname + '-' + datetimestamp + '.' + file.originalname.split('.')[file.originalname.split('.').length - 1]);
+      cb(null, 'uploadedRestore' + '.' + file.originalname.split('.')[file.originalname.split('.').length - 1]);
     }
   });
 
@@ -42,15 +113,8 @@ function uploadRestoreFile( req, res, options ) {
     storage: storage
   }).single('file');
 
+  cb(null, req, res, upload);
 
-  upload(req, res, err => {
-    console.log(req.file);
-    if (err) {
-      res.json({ error_code: 1, err_desc: err });
-      return;
-    }
-    res.json({ error_code: 0, err_desc: null });
-  });
 }
 
 module.exports = MysqlUtilities;
