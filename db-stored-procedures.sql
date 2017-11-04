bc1371922240b93610ed0b4b7eb733a7651fb69d
Marcos Barrera <elyomarcos@gmail.com>
2017-11-04 12:11:09 -0600

    Added procDropAllTables to drop all tables if required. DO NOT USE THIS ON PRODUCTION.

--- db-stored-procedures.sql
+++ db-stored-procedures.sql
@@ -213,3 +213,41 @@ BEGIN
     select concat('*', msg) AS '* DEBUG:';
   END; END IF;
 END $$
+
+
+--- <<< DOT NOT USE THIS STORED PROCEDURE FOR PRODUCTION >>>
+DELIMITER $$
+USE flex_admin$$
+DROP PROCEDURE IF EXISTS procDropAllTables $$
+ 
+CREATE PROCEDURE procDropAllTables()
+ 
+BEGIN
+    DECLARE table_name VARCHAR(255);
+    DECLARE end_of_tables INT DEFAULT 0;
+
+    DECLARE cur CURSOR FOR
+        SELECT t.table_name 
+        FROM information_schema.tables t 
+        WHERE t.table_schema = DATABASE() AND t.table_type='BASE TABLE';
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET end_of_tables = 1;
+
+    SET FOREIGN_KEY_CHECKS = 0;
+    OPEN cur;
+
+    tables_loop: LOOP
+        FETCH cur INTO table_name;
+
+        IF end_of_tables = 1 THEN
+            LEAVE tables_loop;
+        END IF;
+
+        SET @s = CONCAT('DROP TABLE IF EXISTS ' , table_name);
+        PREPARE stmt FROM @s;
+        EXECUTE stmt;
+
+    END LOOP;
+  
+    CLOSE cur;
+    SET FOREIGN_KEY_CHECKS = 1;
+END$$
