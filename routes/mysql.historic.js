33bd383179a80b01b0f5130762b04666b48fddcf
Marcos Barrera Del Río <marcos@softwareallies.com>
2017-11-20 21:33:59 -0600

    Added proper handling of the req.body object in order to get the initial and final date to generate the historic process.

--- routes/mysql.historic.js
+++ routes/mysql.historic.js
@@ -2,21 +2,14 @@ const router = require('express').Router()
 const MySQLHistoricService = require('../services/mysql.historic.service');
 
 router
-    // Esta ruta es nomas para que pruebes con un GET gg, luego se va a eliminar
-    .get('/', (req, res) => {
-        MySQLHistoricService.generateHistoric( '2017-01-01', '2017-12-31' ,(error, data) => {
-            console.log('Error:', error)
-            console.log('Result:', data)
-            return MySQLHistoricService.response(res, error, data); 
-        })
-    })
-    // Este sería el que debes consumir
-    .post('/:start/:end', (req, res) => {
-        const startDate = req.params.start;
-        const endDate = req.params.end;
+    
+    .post('/', (req, res) => {
+        const startDate = require('moment')(req.body.initialDate).format('YYYY-MM-DD');
+        const endDate = require('moment')(req.body.finalDate).format('YYYY-MM-DD');
         MySQLHistoricService.generateHistoric( startDate, endDate ,(error, data) => {
             return MySQLHistoricService.response(res, error, data); 
         })
+        
     })
 
 module.exports = router;
